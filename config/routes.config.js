0acc55303bee5ab6f79d80576a4ec2e43d7035ca
Pedro Frias <plfriasf@gmail.com>
2021-04-18 11:09:27 -0700

    first commit

---
+++ config/routes.config.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require("../controllers/users.controller");
+const productsController = require("../controllers/products.controller");
+
+router.get("/");
+
+// Users
+
+router.get("/register");
+router.post("/register");
+router.get("/login");
+router.post("/login");
+router.get("/activate/:token");
+router.post("/logout");
+router.get("/profile");
+router.get("/wishlist");
+router.get("/users");
+
+// Property
+router.get("/property/create");
+router.post("/property/create");
+router.get("/products/:id");
+router.get("/property/:id/edit");
+router.post("/property/:id/edit");
+router.get("/property/:id/delete");
+
+// Likes
+router.get("/property/:propertyId/like");
+router.get("/property/match");
+router.get("/property/crush");
+
+module.exports = router;
