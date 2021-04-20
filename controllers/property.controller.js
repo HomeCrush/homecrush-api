1762f5a59087ba013bad0102f16a294034c4c794
andrea morales <amoralesp88@gmail.com>
2021-04-20 21:05:54 +0200

    working create logic

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -8,72 +8,88 @@ const Like = require("../models/Like.model");
 
 
 module.exports.create = (req, res, next) => {
-    res.render("properties/create");
-};
-  
-module.exports.doCreate = (req, res, next) => {
-    function renderWithErrors(errors) {
-      res.status(400).json("properties/create", {
-        errors: errors,
-        product: req.body,
-      });
-    }
-  
+  req.body.user = req.currentUser;
+
     Property.create(req.body)
-    .then((product) => res.status(201).json(product))
-      .catch((e) => {
-        if (e instanceof mongoose.Error.ValidationError) {
-          renderWithErrors(e.errors);
-        } else {
-          next(e);
-        }
-
-        /*
-        Property.create(req.body)
-        .then((product) => res.status(201).json(product))
-        .catch(next);
-        */
-      });
+    .then((property) => res.status(201).json(property))
+      .catch((e);
 };
 
-module.exports.edit = (req, res, next) => {
-    Property.findById(req.params.id)
-      .then((property) => {
-        if (
-          !property ||
-          property.owner.toString() !== req.currentUser.id.toString()
-        ) {
-          res.redirect("/");
-        } else {
-          res.json("/properties/${id}/edit", { property });
-        }
-      })
-      .catch((e) => next(e));
+//
+/*module.exports.createProperty = catchAsync(async (req, res, next) => {
+  if (!req.body.owner) req.body.owner = req.user.id;
+  req.body.images = [];
+
+  if (req.files.length !== 0) {
+    req.body.images = req.files.map((element) => element.key);
+  }
+
+  const newProperty = await Property.create(req.body);
+
+  res.status(201).json({
+    status: 201,
+    data: {
+      property: newProperty,
+    },
+  });
+});*/
+
+//
+
+module.exports.update = (req, res, next) => {
+  if (req.file) {
+    req.body.images = req.file.path;
+  }
+
+  Property.findById(req.params.id)
+    .then((property) => {
+      if (!property) {
+        next(createError(404));
+        return;
+      }
+      if (property.owner.toString() !== req.currentUser.toString()) {
+        next(createError(403));
+        return;
+      }
+    return property.save().then(() => res.json({/*property*/}));
+    })
+    .catch(next);
 };
-  
-module.exports.doEdit = (req, res, next) => {
-    function renderWithErrors(errors) {
-      res.status(400).json("/properties/${id}/edit", {
-        errors: errors,
-        product: req.body,
-      });
-    }
-    Product.findById(req.params.id)
-      .then((property) => {
-        if (!property || property.owner.toString() !== req.currentUser.id.toString()) {
-          res.redirect("/");
-        } else {
-          Object.entries(req.body).forEach(([k, v]) => (property[key] = value));
-          return property.save().then(() => {
-            res.json(`/properties/${req.params.id}`);
-          });
-        }
-      })
-      .catch((e) => {
-        if (e instanceof mongoose.Error.ValidationError) {
-          renderWithErrors(e.errors);
-        } else {
-          next(e);
-        }
-      });
-};
+//
+/*exports.updateProperty = catchAsync(async (req, res, next) => {
+  if (req.files.length !== 0) {
+    req.body.images = [];
+    req.body.images = req.files.map((element) => element.key);
+  }
+
+  const property = await Property.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  }); 
+  //Arg "{new: true}" to return the modified document rathen than the original
+  //Arg "runValidators: true" validate the update operation against the model's schema
+  if (!property) {
+    return next('No property found with that ID', 404);
+  }
+
+  res.status(200).json({
+    status: 200,
+    data: {
+      property,
+    },
+  });
+});*/
+//
+
+module.exports.deleteProperty = catchAsync(async (req, res, next) => {
+  const property = await Property.findByIdAndDelete(req.params.id);
+
+  if (!property) {
+    return next('No listing found with that ID', 404);
+  }
+
+  res.status(204).json({
+    status: 204,
+    data: null,
+  });
+});
